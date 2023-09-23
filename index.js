18ef1d034b0350be5526b8e1472e00ba6406007e
souhad <souhadmoussa86@gmail.com>
2023-09-23 20:00:21 +0300

    Step 8

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// step 2
 const express = require('express')
 const app = express()
 app.listen(3000)
@@ -7,47 +7,43 @@ app.get('/', (req, res) => {
     res.send('OK');
   })
 
+//  // step 3 
+//   app.get('/test', (req, res) => {
+//     res.status(200).send("ok");
+//   })
+//  // step 3 
+//   app.get('/time',(req, res) => {
+    
+//     let today=new Date();
 
-//   app.get('/', (req, res) => {
-//     res.status(200).json("ok");
+//     var time=today.getHours() + ":" + today.getSeconds()
+
+//     res.status(200).send(`${time}`);
+
+//     // res.json({status:200,data:time});
+
+//   })
+//  // step 4 
+//   app.get('/hello/:Id?/', (req, res) => {
+//      let Id = req.params.Id || "souhad";
+//      res.status(200).json({status:200, message: "hello",id:Id});
+//   //   res.send(req.params)
 //   })
 
-  app.get('/test', (req, res) => {
-    res.status(200).send("ok");
-  })
+// //STEP 4 */ http://localhost:3000/search?s=souhad*/
 
-  app.get('/time',(req, res) => {
-    
-    let today=new Date();
+//   app.get('/search',(req,res) => {
+//     if(req.query.s){
+//     res.status(200).json({status:200, message: "ok",data:req.query.s});
+//     } else 
+//      res.status(500).json({status:500, message: "you have to provide a search",error:true});
+//   });
+//   //step 5 
 
-    var time=today.getHours() + ":" + today.getSeconds()
-
-    res.status(200).send(`${time}`);
-
-    // res.json({status:200,data:time});
-
-  })
- // step 4 
-  app.get('/hello/:Id?/', (req, res) => {
-     let Id = req.params.Id || "souhad";
-     res.status(200).json({status:200, message: "hello",id:Id});
-  //   res.send(req.params)
-  })
-
-//STEP 4 */ http://localhost:3000/search?s=souhad*/
-
-  app.get('/search',(req,res) => {
-    if(req.query.s){
-    res.status(200).json({status:200, message: "ok",data:req.query.s});
-    } else 
-     res.status(500).json({status:500, message: "you have to provide a search",error:true});
-  });
-  //step 5 
-
-  app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    next();
-  });
+//   app.use((req, res, next) => {
+//     res.setHeader('Content-Type', 'text/html; charset=utf-8');
+//     next();
+//   });
 
 
   const movies = [ { title: 'Jaws', year: 1975, rating: 8 }, 
@@ -129,3 +125,42 @@ app.get('/', (req, res) => {
           return null; 
         }
       }
+
+          //  step 8  // http://localhost:3000/movies/add?title=souhad&year=2003&rating=8
+          app.get("/movies/add", (req, res) => {
+            // /movies/add?title=Red notice&year=2000&rating=8
+            let { title, year, rating } = req.query;
+            let newMovie = {
+                    title: title,
+                    year: year,
+                    rating: parseFloat(rating) || 4
+                 };
+            if (newMovie.year.length !== 4 || isNaN(parseInt(newMovie.year))) {
+              res.status(403).json({
+                status: 403,
+                error: true,
+                message:
+                  "you cannot create a movie without providing a title and a year",
+              });
+            }
+          
+            if (!newMovie.title) {
+              return res.status(403).json({
+                status: 403,
+                error: true,
+                message: "You cannot create a movie without providing a title.",
+              });
+            }
+            if (newMovie.title && newMovie.year && newMovie.rating) {
+              let newList = movies.push(newMovie);
+              res.json(newMovie); 
+          
+              req.newList = newList; 
+              
+            }
+          });
+          app.get("movies/read", (req, res) => {
+            const newList = req.newList; 
+            res.json(newList);
+          });
+        
