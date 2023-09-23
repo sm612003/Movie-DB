f27061a627bed1041bce0a0519a7abe162198451
souhad <souhadmoussa86@gmail.com>
2023-09-23 13:44:54 +0300

    Step 5

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const app = express()
 app.listen(3000)
@@ -41,3 +42,41 @@ app.get('/', (req, res) => {
     } else 
      res.status(500).json({status:500, message: "you have to provide a search",error:true});
   });
+  //step 5 
+
+  app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    next();
+  });
+
+
+  const movies = [ { title: 'Jaws', year: 1975, rating: 8 }, 
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+   { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 } ]
+
+  app.get('/movies/create',(req,res) => {
+    res.send('create');
+
+  })
+  app.get('/movies/read',(req,res) => {
+    res.status(200).json({status:200, data:movies});
+  })
+  app.get('/movies/update',(req,res) => {
+    res.send('update');
+
+  })
+  app.get('/movies/delete',(req,res) => {
+    res.send('delete');
+
+
+  })
+
+  // app.use("/movies", async function (req, res) {
+  //     let responsePayload;
+  //   try {const unwatchedMoviesSelectQuery = "select * from movies where watched = false order by id desc"; 
+  //      responsePayload = await client.execute(unwatchedMoviesSelectQuery); }
+  //     catch (error) { 
+  //     console.log({ error }); 
+  //   responsePayload = null;  }  return res.json(responseDataAdapter(responsePayload));
+  //  });
