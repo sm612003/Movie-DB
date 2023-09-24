c113c74774fbb423454fb1cc742470403e655443
souhad <souhadmoussa86@gmail.com>
2023-09-24 10:22:12 +0300

    Step 9

--- index.js
+++ index.js
@@ -7,43 +7,43 @@ app.get('/', (req, res) => {
     res.send('OK');
   })
 
-//  // step 3 
-//   app.get('/test', (req, res) => {
-//     res.status(200).send("ok");
-//   })
-//  // step 3 
-//   app.get('/time',(req, res) => {
+ // step 3 
+  app.get('/test', (req, res) => {
+    res.status(200).send("ok");
+  })
+ // step 3 
+  app.get('/time',(req, res) => {
     
-//     let today=new Date();
+    let today=new Date();
 
-//     var time=today.getHours() + ":" + today.getSeconds()
+    var time=today.getHours() + ":" + today.getSeconds()
 
-//     res.status(200).send(`${time}`);
+    res.status(200).send(`${time}`);
 
-//     // res.json({status:200,data:time});
+    // res.json({status:200,data:time});
 
-//   })
-//  // step 4 
-//   app.get('/hello/:Id?/', (req, res) => {
-//      let Id = req.params.Id || "souhad";
-//      res.status(200).json({status:200, message: "hello",id:Id});
-//   //   res.send(req.params)
-//   })
+  })
+ // step 4 
+  app.get('/hello/:Id?/', (req, res) => {
+     let Id = req.params.Id || "souhad";
+     res.status(200).json({status:200, message: "hello",id:Id});
+  //   res.send(req.params)
+  })
 
-// //STEP 4 */ http://localhost:3000/search?s=souhad*/
+//STEP 4 */ http://localhost:3000/search?s=souhad*/
 
-//   app.get('/search',(req,res) => {
-//     if(req.query.s){
-//     res.status(200).json({status:200, message: "ok",data:req.query.s});
-//     } else 
-//      res.status(500).json({status:500, message: "you have to provide a search",error:true});
-//   });
-//   //step 5 
-
-//   app.use((req, res, next) => {
-//     res.setHeader('Content-Type', 'text/html; charset=utf-8');
-//     next();
-//   });
+  app.get('/search',(req,res) => {
+    if(req.query.s){
+    res.status(200).json({status:200, message: "ok",data:req.query.s});
+    } else 
+     res.status(500).json({status:500, message: "you have to provide a search",error:true});
+  });
+  //step 5 
+// used for the arabic word 
+  app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    next();
+  });
 
 
   const movies = [ { title: 'Jaws', year: 1975, rating: 8 }, 
@@ -164,3 +164,22 @@ app.get('/', (req, res) => {
             res.json(newList);
           });
         
+
+           //step 9 
+           app.get('/movies/delete/:id', (req, res) => {
+            const idToDelete = parseInt(req.params.id);
+          
+            
+            // Find the index of the movie to delete
+            const indexToDelete = idToDelete - 1;
+          
+            if (indexToDelete >= 0 && indexToDelete < movies.length) {
+              // Use splice to remove the movie from the array
+              movies.splice(indexToDelete, 1);
+          
+              res.status(200).json({ status: 200, message: `Movie with ID ${idToDelete} deleted.`, data:movies});
+            } else {
+              res.status(404).json({ status: 404, message: `Movie with ID ${idToDelete} not found.`, error: true });
+            }
+          });
+          
